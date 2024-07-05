89ad9aeebd6c435aa8d18484991dfe626672560e
amandamigliorini <amanda.migliorini@hotmail.com>
2024-07-04 22:09:40 -0300

    Team Activity Added

--- routes/accountRoute.js
+++ routes/accountRoute.js
@@ -14,6 +14,16 @@ router.post(
     regValidate.registationRules(),
     regValidate.checkRegData,
     utilities.handleErrors(accountController.registerAccount)
-  )
+)
+
+// Process the login attempt
+router.post(
+    "/login",
+    regValidate.LoginRules(),
+    regValidate.checkLoginData,
+    (req, res) => {
+      res.status(200).send('login process')
+    }
+)
 
 module.exports = router;
